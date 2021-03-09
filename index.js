453deb74d4f02a7d730a6f9136c26ae308f24e5f
Raul Bunicenha <>
2021-03-09 20:20:23 -0300

    Data Types

---
+++ index.js
@@ -0,0 +1,10 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number and object
+*/
+
+var myName = 'Raul'
+myName = 8
+let ourName = 'freeCodeCamp'
+const pi = 3.14
+
+console.log(myName, ourName, pi);
